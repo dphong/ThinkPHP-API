6173ffa10edbdb6505c7a279f0fdd6fd9b368bb4
hdp <386392758@qq.com>
2018-04-06 16:56:08 +0800

    update composer

--- thinkphp/library/think/view/driver/Php.php
+++ thinkphp/library/think/view/driver/Php.php
@@ -2,7 +2,7 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
@@ -21,12 +21,16 @@ class Php
 {
     // 模板引擎参数
     protected $config = [
+        // 视图基础目录（集中式）
+        'view_base'   => '',
         // 模板起始路径
         'view_path'   => '',
         // 模板文件后缀
         'view_suffix' => 'php',
         // 模板文件名分隔符
         'view_depr'   => DS,
+        // 默认模板渲染规则 1 解析为小写+下划线 2 全部转换小写
+        'auto_rule'   => 1,
     ];
 
     public function __construct($config = [])
@@ -109,25 +113,34 @@ class Php
             $this->config['view_path'] = App::$modulePath . 'view' . DS;
         }
 
+        $request = Request::instance();
+        // 获取视图根目录
         if (strpos($template, '@')) {
+            // 跨模块调用
             list($module, $template) = explode('@', $template);
-            $path                    = APP_PATH . $module . DS . 'view' . DS;
+        }
+        if ($this->config['view_base']) {
+            // 基础视图目录
+            $module = isset($module) ? $module : $request->module();
+            $path   = $this->config['view_base'] . ($module ? $module . DS : '');
         } else {
-            $path = $this->config['view_path'];
+            $path = isset($module) ? APP_PATH . $module . DS . 'view' . DS : $this->config['view_path'];
         }
 
-        // 分析模板文件规则
-        $request    = Request::instance();
-        $controller = Loader::parseName($request->controller());
-        if ($controller && 0 !== strpos($template, '/')) {
-            $depr     = $this->config['view_depr'];
-            $template = str_replace(['/', ':'], $depr, $template);
-            if ('' == $template) {
-                // 如果模板文件名为空 按照默认规则定位
-                $template = str_replace('.', DS, $controller) . $depr . $request->action();
-            } elseif (false === strpos($template, $depr)) {
-                $template = str_replace('.', DS, $controller) . $depr . $template;
+        $depr = $this->config['view_depr'];
+        if (0 !== strpos($template, '/')) {
+            $template   = str_replace(['/', ':'], $depr, $template);
+            $controller = Loader::parseName($request->controller());
+            if ($controller) {
+                if ('' == $template) {
+                    // 如果模板文件名为空 按照默认规则定位
+                    $template = str_replace('.', DS, $controller) . $depr . (1 == $this->config['auto_rule'] ? Loader::parseName($request->action(true)) : $request->action());
+                } elseif (false === strpos($template, $depr)) {
+                    $template = str_replace('.', DS, $controller) . $depr . $template;
+                }
             }
+        } else {
+            $template = str_replace(['/', ':'], $depr, substr($template, 1));
         }
         return $path . ltrim($template, '/') . '.' . ltrim($this->config['view_suffix'], '.');
     }
