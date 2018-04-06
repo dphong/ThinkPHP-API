7ca9a4977c691baafb48c4eb683e13a5f8a3b06a
hdp <386392758@qq.com>
2018-04-06 12:55:26 +0800

    代码功能测试

--- application/api/controller/Map.php
+++ application/api/controller/Map.php
@@ -28,8 +28,10 @@ class Map extends Controller{
         parent::__construct();
         $myclass = new Myclass();
         if($myclass->is_https()) {
+            echo 'https';die;
             $this->view->replace(['__PUBLIC__'    =>  'https://static.whark.cn',]);
         } else {
+            echo 'http';die;
             $this->view->replace(['__PUBLIC__'    =>  'http://ongjgltez.bkt.clouddn.com',]);
         }
     }
